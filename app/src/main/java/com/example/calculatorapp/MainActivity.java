fd9d8b3f7fd86f63ad0041cb07d00adea9c1ba9f
lw044 <113703225+lw044@users.noreply.github.com>
2023-09-07 11:04:59 -0500

    Created displayOperation method and got it to work
    By clicking any of the operation buttons, the textView in the middle changes to the symbol of the button that is pressed

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // hi
+    }
+
+    //https://stackoverflow.com/questions/16611198/how-to-know-which-button-called-a-method
+    // Code from StackOverflow that shows which button called this method when many buttons share
+    // this method; the button is considered the view in the parameter and can be accessed by
+    // accessing the view
+    public void displayOperation(View view){
+        int button = view.getId();
+        Button b = findViewById(button);
+        String operation = b.getText().toString();
+        TextView t = findViewById(R.id.mathOperationTextView);
+        t.setText(operation);
     }
 }
