ace45286bff789394e196537119c4cdbde6e078a
lw044 <113703225+lw044@users.noreply.github.com>
2023-09-08 11:05:08 -0500

    Created a method that can correctly complete and display calculations
    Was able to get the code to correctly display operations and complete accurate computations, which is called when the equals button is pushed

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -17,15 +17,57 @@ public class MainActivity extends AppCompatActivity {
         // hi
     }
 
-    //https://stackoverflow.com/questions/16611198/how-to-know-which-button-called-a-method
-    // Code from StackOverflow that shows which button called this method when many buttons share
-    // this method; the button is considered the view in the parameter and can be accessed by
-    // accessing the view
+
     public void displayOperation(View view){
+        //https://stackoverflow.com/questions/16611198/how-to-know-which-button-called-a-method
+        // Code from StackOverflow that shows which button called this method when many buttons share
+        // this method; the button is considered the view in the parameter and can be accessed by
+        // accessing the view
         int button = view.getId();
         Button b = findViewById(button);
         String operation = b.getText().toString();
         TextView t = findViewById(R.id.mathOperationTextView);
         t.setText(operation);
+    }
+
+    public void solveEquation(View v) {
+        //https://www.freecodecamp.org/news/java-string-to-int-how-to-convert-a-string-to-an-
+        // integer/
+        // Code from Free Code Camp that shows how to change a string into an double; calls a
+        // method from the Double class to do so
+        EditText firstNumET = findViewById(R.id.firstNumberEditView);
+        EditText secondNumET = findViewById(R.id.secondNumberEditView);
+
+        if (!firstNumET.getText().toString().equals("") &&
+                !secondNumET.getText().toString().equals("")) {
+            double firstNum = Double.parseDouble(firstNumET.getText().toString());
+            double secondNum = Double.parseDouble(secondNumET.getText().toString());
+
+            TextView operationTV = findViewById(R.id.mathOperationTextView);
+            String operation = operationTV.getText().toString();
+
+            TextView solution = findViewById(R.id.solutionTextView);
+            solution.setText(operation);
+            if (operation.equals("")) {
+                //https://stackoverflow.com/questions/8102741/how-to-use-string-resource-in-a-java-
+                //class-in-android
+                // Code from Stack Overflow, showing how to use a string resource in an xml file in
+                // java code; accesses the string by searching for its id
+                solution.setText(this.getString(R.string.no_operation_chosen_message));
+            } else {
+                double ansNum = 0;
+                if (operation.equals("+")) {
+                    ansNum = firstNum + secondNum;
+                } else if (operation.equals("-")) {
+                    ansNum = firstNum - secondNum;
+                } else if (operation.equals("x")) {
+                    ansNum = firstNum * secondNum;
+                } else if (operation.equals("/")) {
+                    ansNum = firstNum / secondNum;
+                }
+                String answer = "" + ansNum;
+                solution.setText(answer);
+            }
+        }
     }
 }
