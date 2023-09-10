6d7d460773f4e5f3329788c980b8d073c89a7fc3
lw044 <113703225+lw044@users.noreply.github.com>
2023-09-10 13:56:44 -0500

    Ran into a strange bug, attempting to fix it by deleting the file now

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
